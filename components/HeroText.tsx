d231d18f281da9b333ec6f859eb661c80d5fb83c
shelvy <sks91@berkeley.edu>
2022-12-30 19:23:32 -0800

    remove unused asset

--- components/HeroText.tsx
+++ components/HeroText.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/HeroText.module.css';
 const HeroText: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>Hi, I'm Shelvin</h1>
+      <h1 className={styles.heading}>Hi, I&apos;m Shelvin</h1>
       <h2 className={styles.secondary}>Software Engineer at EasyPost</h2>
       <p className={styles.subtext}>
         I build (web) applications with delightful user experiences backed by
